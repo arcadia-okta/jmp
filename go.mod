65a0c1bda3dc2e4aafab589431cd5ae8b7303938
Cady <arcadia.rose@okta.com>
2023-09-26 10:49:49 -0400

    Loosely fuzzy-find git repositories

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module jmp
 
 go 1.21.0
+
+require (
+	github.com/lithammer/fuzzysearch v1.1.8 // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
--- jmp.go
+++ jmp.go
@@ -5,46 +5,82 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+
+	"github.com/lithammer/fuzzysearch/fuzzy"
 )
 
-func findDir(start, target string, maxDepth int) (string, error) {
+func isGitRepo(path string) (bool, error) {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return false, err
+	}
+	for _, file := range files {
+		if file.Name() == ".git" && file.IsDir() {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func findGitRepos(start string, maxDepth int) ([]string, error) {
+	var dirs []string = []string{}
+
 	if maxDepth == -1 {
-		return "", nil
+		return dirs, nil
 	}
 
 	files, err := ioutil.ReadDir(start)
 	if err != nil {
-		return "", err
+		return dirs, err
 	}
 
-	var dirs []string = make([]string, 1)
 	for _, file := range files {
+		pathToFile := path.Join(start, file.Name())
 		if file.IsDir() {
-			// TODO - Use a fuzzy match.
-			if file.Name() == target {
-				return path.Join(start, file.Name()), nil
+			isRepo, err := isGitRepo(pathToFile)
+			if err != nil {
+				return dirs, err
+			} else if isRepo {
+				dirs = append(dirs, pathToFile)
 			} else {
-				dirs = append(dirs, file.Name())
+				repos, err := findGitRepos(pathToFile, maxDepth-1)
+				if err != nil {
+					return dirs, err
+				}
+				dirs = append(dirs, repos...)
 			}
 		}
 	}
 
-	for _, dir := range dirs {
-		nextDir := path.Join(start, dir)
-		found, err := findDir(nextDir, target, maxDepth-1)
-		if err == nil && found != "" {
-			return found, nil
+	return dirs, nil
+}
+
+func fuzzyFindGitRepo(startDir, name string) (string, error) {
+	gitRepos, err := findGitRepos(startDir, 5)
+	if err != nil {
+		return "", nil
+	}
+
+	bestPath := ""
+	bestScore := -1
+
+	for _, repoPath := range gitRepos {
+		_, directoryName := path.Split(repoPath)
+		score := fuzzy.RankMatch(name, directoryName)
+		if score > bestScore {
+			bestPath = repoPath
+			bestScore = score
 		}
 	}
 
-	return "", nil
+	return bestPath, nil
 }
 
 func main() {
 	start := "/Users/arcadia.rose/Code"
 	targetRepo := os.Args[1]
 
-	path, err := findDir(start, targetRepo, 5)
+	path, err := fuzzyFindGitRepo(start, targetRepo)
 	found := err == nil && path != ""
 	if found {
 		fmt.Printf("Found %s\n", path)
